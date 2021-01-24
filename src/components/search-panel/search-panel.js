b8c3ae54695654d5d3ca37a0b05dcbacd03400a2
SyrgakRavshanbekUulu <75747709+SyrgakRavshanbekUulu@users.noreply.github.com>
2021-01-24 15:55:27 +0600

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    }
+    onUpdateSearch(e) {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+    render() {
+        return (
+            <input
+            className="form-control search-input"
+            type="text"
+            placeholder="poisk po zapis"
+            onChange={this.onUpdateSearch}
+            />
+        )
+    }
+}
+
